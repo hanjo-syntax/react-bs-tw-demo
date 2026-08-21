5585d13abf5035735d22b6e0f13951806f5578db
hanjo-syntax <hanjo.winter@tn.syntax-institut.de>
2026-08-21 09:29:01 +0200

    Fix: Imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+/* 1. Bootstrap CSS */
+@import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* 2. Tailwind Import mit Präfix */
+@import "tailwindcss" prefix(tw);
 import './index.css'
 import App from './App.jsx'
 
